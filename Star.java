9d3af8739b153319fb38d5b973451d48addd4ba8
Yashwant shelokar <shelokr.yashwant@gmil.com>
2022-02-15 12:47:30 +0530

    basic program for Star printing

---
+++ Star.java
@@ -0,0 +1,25 @@
+package Star;
+
+public class Star {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) 
+		{
+
+			for (int j = 5; j > i; j--) {
+
+				System.out.print(" ");
+
+		    	}
+			for (int k = 1; k <= i; k++) {
+
+				System.out.print("*");
+
+			   }
+			System.out.println();
+
+		}
+
+	}
+
+}
